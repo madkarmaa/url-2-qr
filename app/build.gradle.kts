2701d0c5a1b476fac502b852b7fe74fd73b04687
madkarmaa <madkarma99@gmail.com>
2024-12-15 20:19:13 +0100

    ci: update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,20 +20,23 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
     kotlinOptions {
         jvmTarget = "17"
     }
+
     buildFeatures {
         compose = true
     }
